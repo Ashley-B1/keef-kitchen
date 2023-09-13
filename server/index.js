910868712b608f1fef38bf0f14076524a5fec6dc
Ashley (Skye) Brown <stormi.seuneu@outlook.com>
2023-09-13 13:00:29 -0500

    idk

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(express.json());
 
 app.post(("/checkout"), async (req, res) => {
@@ -81,7 +81,7 @@ const generateDescription = item => {
 };
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
 app.listen(Number(process.env.PORT), () => console.log(`Listening in port ${process.env.PORT}!`));
